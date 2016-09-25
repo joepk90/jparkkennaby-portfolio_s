3e618e51f23b2424d7726967147c070996653a13
Joe Park-Kennaby <jparkkennaby@gmail.com>
2016-09-25 17:23:17 +0100

    wp post loop update

--- archive.php
+++ archive.php
@@ -13,7 +13,7 @@ get_header(); ?>
 	
 		<main id="main" class="site-main" role="main">
 
-		<?php query_posts( 'posts_per_page=-1' );
+		<?php
 		if ( have_posts() ) : ?>
 
 			<header class="page-header">
